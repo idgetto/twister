ab271b832f69f107fc0a29e4967a620f305e82b4
idgetto <isaac.getto@gmail.com>
2014-05-10 17:15:39 -0500

    move code into BoardView

--- src/com/moonshine/twister/BoardView.java
+++ src/com/moonshine/twister/BoardView.java
@@ -3,6 +3,11 @@ package com.moonshine.twister;
 import android.widget.GridView;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import static android.view.MotionEvent.*;
+import android.view.View.OnTouchListener;
+import android.view.View;
+import android.widget.Toast;
 
 public class BoardView extends GridView {
 
@@ -10,21 +15,56 @@ public class BoardView extends GridView {
 
 	public BoardView(Context context) {
     super(context);
+    System.out.println("Inside boardView constructor");
     init(context);
   }
 
   public BoardView(Context context, AttributeSet attrs) {
     super(context, attrs);
+    System.out.println("Inside boardView constructor");
     init(context);
   }
 
   public BoardView(Context context, AttributeSet attrs, int defStyle) {
     super(context, attrs, defStyle);
+    System.out.println("Inside boardView constructor");
     init(context);
   }
 
   private void init(Context context) {
     this.context = context;
+    setAdapter(new BoardAdapter(context, this));
+
+    final Context aContext = context;
+    setOnTouchListener(new OnTouchListener() {
+
+      public boolean onTouch(View v, MotionEvent event) {
+
+        int eventX = (int) event.getX();
+        int eventY = (int) event.getY();
+        int index = pointToPosition(eventX, eventY);
+        Circle circle = (Circle) getItemAtPosition(index);
+        System.out.println("Event!!!" + index);
+
+        switch (event.getAction()) {
+
+          case ACTION_DOWN:
+              Toast.makeText(aContext, "Touched circle at index: " + index, Toast.LENGTH_SHORT).show();
+            // textView.setText("Touched circle at index: " + index);
+            break;
+
+          case ACTION_UP:
+              Toast.makeText(aContext, "Let go of circle at index: " + index, Toast.LENGTH_SHORT).show();
+            // textView.setText("Let go of circle at index: " + index);
+            break;
+
+        }
+
+        return true;
+
+      }
+
+    });
   }
 
 }
--- src/com/moonshine/twister/GameActivity.java
+++ src/com/moonshine/twister/GameActivity.java
@@ -3,18 +3,11 @@ package com.moonshine.twister;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.TextView;
-import android.widget.AdapterView;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.widget.Toast;
-import android.content.Context;
-import android.view.MotionEvent;
-import static android.view.MotionEvent.*;
 import android.widget.GridView;
 
 public class GameActivity extends Activity {
 
-  private GridView boardView;
+  private BoardView boardView;
   private GridView moveView;
   private TextView textView;
 
@@ -28,47 +21,14 @@ public class GameActivity extends Activity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.game_activity);
 
-      boardView = (GridView) findViewById(R.id.board_view);
+      System.out.println("Just before findViewById");
+      boardView = (BoardView) findViewById(R.id.board_view);
       moveView  = (GridView) findViewById(R.id.move_view);
       textView  = (TextView) findViewById(R.id.text_view);
 
       playerOne = new Player();
       playerTwo = new Player();
       currentPlayer = playerOne;
-
-      boardView.setAdapter(new BoardAdapter(this, boardView));
-
-      final Context context = this;
-
-      boardView.setOnTouchListener(new OnTouchListener() {
-
-        public boolean onTouch(View v, MotionEvent event) {
-
-          int eventX = (int) event.getX();
-          int eventY = (int) event.getY();
-          int index = boardView.pointToPosition(eventX, eventY);
-          Circle circle = (Circle) boardView.getItemAtPosition(index);
-          System.out.println("Event!!!" + index + " " + event.getAction());
-
-          switch (event.getAction()) {
-
-            case ACTION_DOWN:
-                // Toast.makeText(context, "Touched circle at index: " + index, Toast.LENGTH_SHORT).show();
-              textView.setText("Touched circle at index: " + index);
-              break;
-
-            case ACTION_UP:
-                // Toast.makeText(context, "Let go of circle at index: " + index, Toast.LENGTH_SHORT).show();
-              textView.setText("Let go of circle at index: " + index);
-              break;
-
-          }
-
-          return true;
-
-        }
-
-      });
 
     }
 
