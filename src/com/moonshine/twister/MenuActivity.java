698332ac0f47e3e0df1864220a2cfdf1fd2e805d
idgetto <isaac.getto@gmail.com>
2014-05-09 23:48:39 -0500

    fix onClick issues

--- src/com/moonshine/twister/MenuActivity.java
+++ src/com/moonshine/twister/MenuActivity.java
@@ -3,6 +3,7 @@ package com.moonshine.twister;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends Activity
 {
@@ -12,28 +13,28 @@ public class MenuActivity extends Activity
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.menu_activity);
-    
+
   }
-  
-  public void spClick()
+
+  public void spClick(View view)
   {
 	 Intent spIntent = new Intent(this, GameActivity.class);
 	 spIntent.putExtra("single", true);
 	 startActivity(spIntent);
   }
-  
-  public void tpClick()
+
+  public void tpClick(View view)
   {
 	  Intent tpIntent = new Intent(this, GameActivity.class);
 	  tpIntent.putExtra("single", false);
 	  startActivity(tpIntent);
   }
-  
-  public void scoresclick()
+
+  public void scoresClick(View view)
   {
 	  Intent scoresIntent = new Intent(this, ScoresActivity.class);
 	  scoresIntent.putExtra("menu", true);
 	  startActivity(scoresIntent);
   }
-  
+
 }
