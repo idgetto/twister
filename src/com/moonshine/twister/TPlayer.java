503abed0192ccd684e4c278d1836d6ca3bbc9d48
idgetto <isaac.getto@gmail.com>
2014-05-23 10:23:34 -0500

    add comments

--- src/com/moonshine/twister/TPlayer.java
+++ src/com/moonshine/twister/TPlayer.java
@@ -1,3 +1,11 @@
+/**
+ * TPlayer.java
+ * Represets a human player and 
+ * manages their score and touches
+ * @author: Kevin You, Isaac Getto
+ * Period: 4
+ * Date: 05-22-14
+ */
 package com.moonshine.twister;
 
 import java.util.HashMap;
@@ -9,7 +17,11 @@ public class TPlayer {
 	private HashSet<Integer> pressed;
 	public MoveView moveView;
 	private int score;
-
+	
+	/**
+	 * Creates a new TPplayer
+	 * @param moveView the MoveView associated with this TPlayer
+	 */ 
 	public TPlayer(MoveView moveView) {
 		lastPressed = new HashMap<Finger, Integer>();
 		pressed = new HashSet<Integer>();
@@ -17,33 +29,65 @@ public class TPlayer {
 		score = 0;
 	}
 
+	/**
+	 * Returns the player's score
+	 * @return the player's score
+	 */ 
 	public int getScore() {
 		return score;
 	}
 
+	/**
+	 * Sets the player's score
+	 * @param points the score to set
+	 */ 
 	public void setScore(int points) {
 		score = points;
 	}
 
+	/**
+	 * Increments the player's score
+	 */ 
 	public void incrementScore() {
 		score++;
 	}
-
+	
+	/**
+	 * Returns the player's MoveView
+	 * @return the MoveView 
+	 */ 
 	public MoveView getMoveView() {
 		return moveView;
 	}
 
+	/**
+	 * Checks if the player is already
+	 * using a given circle
+	 * @param finger the finger attempting to press a circle
+	 * @param index the circle index being touched
+	 * @return whether the circle is in use
+	 */ 
 	public boolean using(Finger finger, int index) {
 		if (lastPressed.containsKey(finger) && lastPressed.get(finger) == index)
 			return true;
 		return pressed.contains(index);
 	}
 
+	/**
+	 * Adds the pressed circle to the player's
+	 * list of used circles
+	 * @param finger the finger pressing the circle
+	 * @param index the circle index being touched
+	 */ 
 	public void press(Finger finger, int index) {
 		lastPressed.put(finger, index);
 		pressed.add(index);
 	}
 
+	/**
+	 * Removes the circle from the player's
+	 * list of used circle
+	 */ 
 	public void release(int index) {
 		pressed.remove(index);
 	}
